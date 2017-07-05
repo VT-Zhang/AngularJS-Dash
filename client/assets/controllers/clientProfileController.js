9c620847d34d5a41478c68cc9bfc38f8529cd2bd
VT-Zhang <jianz@vt.edu>
2017-07-05 15:23:43 -0400

    Added data table for client profile

--- client/assets/controllers/clientProfileController.js
+++ client/assets/controllers/clientProfileController.js
@@ -1,4 +1,4 @@
-app.controller("clientProfileController", ["$scope", "$http", "storesFactory", function($scope, $http, storesFactory){
+app.controller("clientProfileController", ["$scope", "$http", "storesFactory", '$mdEditDialog', '$q', '$timeout', function($scope, $http, storesFactory, $mdEditDialog, $q, $timeout){
 
     var index = function(){
         storesFactory.getAllClients(function(data){
@@ -7,5 +7,19 @@ app.controller("clientProfileController", ["$scope", "$http", "storesFactory", f
     }
 
     index(); 
+
+    $scope.query = {
+        order: 'name',
+        limit: 5,
+        page: 1
+    };
+
+    $scope.limitOptions = [5, 10, 15];
+
+
+    $scope.logPagination = function (page, limit) {
+        console.log('page: ', page);
+        console.log('limit: ', limit);
+    }
 
 }]);
